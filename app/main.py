7c913c939a16e1814184b08a053427de4368287a
Esha Santosh <eshasantosh04@gmail.com>
2024-06-14 10:30:26 +0530

    fixed voting

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import models            #the . represents the current folder that this file is in
 from .database import engine
-from .routers import post, user, auth
+from .routers import post, user, auth, vote
 from .config import settings        #imported instance of the class, not the class itself
 
 models.Base.metadata.create_all(bind=engine)
@@ -11,3 +11,4 @@ app = FastAPI()     # created instance of FastAPI
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
